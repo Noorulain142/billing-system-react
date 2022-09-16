59590abaa82f7c17e232f7321a81c8207cd3128a
Dev <dev@Devs-MacBook-Pro.local>
2022-09-16 13:49:56 +0500

    react app minor changes

--- src/components/PlansShow.js
+++ src/components/PlansShow.js
@@ -4,43 +4,44 @@ import { PageNotFound } from '../pages/Error404'
 
 const PlansShow = ({ plan }) => {
   return (
-    plan === null ? <PageNotFound/>:
-    <div className='mt-5  card col-lg-10 mx-auto'>
-      <div className='card-body'>
-        <p>
-          <strong><h1 className=' text-center card-title'>{plan.name}</h1></strong>
-        </p>
-        <p>
-          <strong><h3 className=' text-center card-subtitle mb-2 text-muted'> monthly fee : {plan.monthly_fee} </h3></strong>
-        </p>
-        <p>
-          <h3>Features:</h3>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item ' style={{ backgroundColor: '#577399' }}>
-              {plan.features.map((feature) => {
-                return (
-                  <p key={feature.id}>
-                    <ul>
-                      <li>name : {feature.name}</li>
-                      <li> code : {feature.code}</li>
-                      <li>unit price : {feature.unit_price}</li>
-                      <li>max unit limit : {feature.max_unit_limit}</li>
-                      <li>usage value : {feature.usage_value}</li>
-                      <li>over use : {feature.over_use}</li>
-                    </ul>
-                  </p>
-                )
-              })}
-            </li>
-          </ul>
-        </p>
-        <Link to='/'>
-          <button className='btn btn-outline-dark btn-sm shadow'>
-            Back
-          </button>
-        </Link>
-      </div>
-    </div>
+    plan === null
+      ? <PageNotFound />
+      : <div className='mt-5  card col-lg-10 mx-auto'>
+        <div className='card-body'>
+          <p>
+            <strong><h1 className=' text-center card-title'>{plan.name}</h1></strong>
+          </p>
+          <p>
+            <strong><h3 className=' text-center card-subtitle mb-2 text-muted'> monthly fee : {plan.monthly_fee} </h3></strong>
+          </p>
+          <p>
+            <h3>Features:</h3>
+            <ul className='list-group list-group-flush'>
+              <li className='list-group-item ' style={{ backgroundColor: '#577399' }}>
+                {plan.features.map((feature) => {
+                  return (
+                    <p key={feature.id}>
+                      <ul>
+                        <li>name : {feature.name}</li>
+                        <li> code : {feature.code}</li>
+                        <li>unit price : {feature.unit_price}</li>
+                        <li>max unit limit : {feature.max_unit_limit}</li>
+                        <li>usage value : {feature.usage_value}</li>
+                        <li>over use : {feature.over_use}</li>
+                      </ul>
+                    </p>
+                  )
+                })}
+              </li>
+            </ul>
+          </p>
+          <Link to='/'>
+            <button className='btn btn-outline-dark btn-sm shadow'>
+              Back
+            </button>
+          </Link>
+        </div>
+        </div>
 
   )
 }
--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import error404 from './error404.png';
+import React from 'react'
+import error404 from './error404.png'
 import { Link } from 'react-router-dom'
 
 export const PageNotFound = () => {
-    return (
-        <div id="wrapper text-center">
-            <img src={error404} height="700" width="700"/>
-            <Link className='nav-link' to='/'> <button className='btn btn-outline-dark btn-sm shadow'>
-                        back
-                      </button></Link>
+  return (
+    <div id='wrapper text-center'>
+      <img src={error404} height='700' width='700' />
+      <Link className='nav-link' to='/'> <button className='btn btn-outline-dark btn-sm shadow'>
+        back
+      </button>
+      </Link>
 
-        </div >
+    </div>
 
-    )
+  )
 }
